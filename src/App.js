13e2083d5a9aa740629675536bd31a442ddff678
jesuscaleb <almostperfect557@gmail.com>
2026-01-08 05:51:58 +0000

    feat(*): navbar and slogan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Menu from './components/Menu/Menu';
+import Slogan from './components/Slogan/Slogan';
 import Portfolio from './components/Portfolio/Portfolio';
 import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
+      <Menu></Menu>
+      <Slogan></Slogan>
       <Welcome></Welcome>
       <Portfolio></Portfolio>
     </div>
